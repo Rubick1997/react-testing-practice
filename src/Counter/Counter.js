f26bfc73663336dcf3a217a91b52292fb0b8b4ae
Rustam <rustam905@gmail.com>
2021-05-14 10:23:48 -0700

    more tests

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Counter.css";
 
 function Counter() {
-    return (
-        <div>
-            <h1 data-testid="header">My Counter</h1>
-        </div>
-    )
+	const [counterValue, setCounterValue] = useState(0);
+	const [inputValue, setInputValue] = useState(1);
+	return (
+		<div>
+			<h1 data-testid='header'>My Counter</h1>
+			<h1 data-testid='counter'>{counterValue}</h1>
+			<button data-testid='subtract-btn'>-</button>
+			<input
+				data-testid='input'
+				type='number'
+				value={inputValue}
+				className='text-center'
+			/>
+			<button data-testid='add-btn'>+</button>
+		</div>
+	);
 }
 
-export default Counter
+export default Counter;
